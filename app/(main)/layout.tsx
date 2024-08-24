f2cb7f98910a07645a857fe4e2a5e7d7d197164f
KaimuSeino <skim07a19yki@icloud.com>
2024-08-24 20:53:49 +0900

    画面全体の配置を設定

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,6 +5,15 @@ const MainLayout = ({
 }) => {
     return (
         <div className="h-full">
+            <div className="h-[70px] fixed inset-y-0 w-full z-50">
+                Navbar
+            </div>
+            <div className="flex pt-[80px] h-full w-56 flex-col fixed inset-y-0 z-50">
+                Sidebar
+            </div>
+            <main className="md:pl-56 pt-[80px] h-full">
+                {children}
+            </main>
         </div>
     )
 }
