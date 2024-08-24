fbe1662dba3b89d112b0d9dab7a2e83a6c9fb5c6
KaimuSeino <skim07a19yki@icloud.com>
2024-08-24 18:35:16 +0900

    メインのプラットフォームのlayoutファイルを作成

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,12 @@
+const MainLayout = ({
+    children
+}: {
+    children: React.ReactNode
+}) => {
+    return (
+        <div className="h-full">
+        </div>
+    )
+}
+
+export default MainLayout
