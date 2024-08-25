a9e7658efa0fca035d07300ce2cd84ad74e3c455
KaimuSeino <skim07a19yki@icloud.com>
2024-08-25 22:13:28 +0900

    ダミーデータの追加

--- app/(main)/(study)/study/[lessonId]/page.tsx
+++ app/(main)/(study)/study/[lessonId]/page.tsx
@@ -8,12 +8,21 @@ const StudyLessonIdPage = ({
     params: { lessonId: string }
 }) => {
 
-    // lessonIdよりlesson情報を取得する予定
-    const lessonInfo = { label: 'easy', time: 120000, words: [
+    // lessonIdでlesson情報を取得する予定
+    const lessonInfo = { label: 'easy', time: 30000, words: [
         {id: 1, english: 'apple', japanese: 'りんご'},
-        {id: 1, english: 'banana', japanese: 'ばなな'},
-        {id: 1, english: 'cherry', japanese: 'さくらんぼ'},
-        {id: 1, english: 'customer', japanese: 'お客さん'}
+        {id: 2, english: 'banana', japanese: 'ばなな'},
+        {id: 3, english: 'cherry', japanese: 'さくらんぼ'},
+        {id: 4, english: 'customer', japanese: 'お客さん'},
+        {id: 5, english: 'president', japanese: '大統領さん'},
+        {id: 6, english: 'copyright', japanese: '著作権'},
+        {id: 7, english: 'employer', japanese: '雇用主'},
+        {id: 8, english: 'receptionist', japanese: '受付'},
+        {id: 9, english: 'engineer', japanese: '技術者'},
+        {id: 10, english: 'clerk', japanese: '事務員'},
+        {id: 11, english: 'court', japanese: '裁判所'},
+        {id: 12, english: 'auditorium', japanese: '講堂'},
+        {id: 13, english: 'appendix', japanese: '付録'}
     ]};
 
     return ( 
