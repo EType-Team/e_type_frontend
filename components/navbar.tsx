326e7c30c8e27ed2674b01a54cee9bdbb0c83219
KaimuSeino <skim07a19yki@icloud.com>
2024-08-25 11:15:41 +0900

    navbarコンポーネントの作成

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,9 @@
+import Logo from "@/components/logo";
+
 const Navbar = () => {
     return ( 
         <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
-
+            <Logo />
         </div>
      );
 }
