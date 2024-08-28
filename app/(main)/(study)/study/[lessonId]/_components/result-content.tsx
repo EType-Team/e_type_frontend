e0af7aab32906a0505306c5478ea507ca4d4a927
KaimuSeino <skim07a19yki@icloud.com>
2024-08-28 21:17:57 +0900

    レッスンの結果を表示するコンポーネントを作成

---
+++ app/(main)/(study)/study/[lessonId]/_components/result-content.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardContent,
+    CardFooter
+} from "@/components/ui/card"
+import Link from "next/link";
+
+const ResultContent = () => {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle className="text-center">
+                    結果
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                どの単語を学習したかを表示する
+            </CardContent>
+            <CardFooter className="justify-center">
+                <Button>
+                    <Link href='/lesson'>
+                        レッスン一覧
+                    </Link>
+                </Button>
+            </CardFooter>
+        </Card>
+    );
+}
+ 
+export default ResultContent;
