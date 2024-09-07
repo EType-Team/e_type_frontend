1d6abd8cfcec70ec8c520ca5b21e5adc01a834bf
KaimuSeino <skim07a19yki@icloud.com>
2024-09-07 19:40:27 +0900

    useWordProgressStoreを作成

---
+++ store/user-word-progress-store.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand"
+
+type EditUserWordProgress = {
+    id: number
+    total_typings: number
+}
+
+type userWordProgressState = {
+    editedUserWordProgress: EditUserWordProgress
+    updateEditedUserWordProgress: (state: EditUserWordProgress) => void
+    resetEditedUserWordProgress: () => void
+}
+
+const useUserWordProgressStore = create<userWordProgressState>((set) => ({
+    editedUserWordProgress: { id: 0, total_typings: 0 },
+    updateEditedUserWordProgress: (state) => set({ editedUserWordProgress: state }),
+    resetEditedUserWordProgress: () => set({ editedUserWordProgress: { id: 0, total_typings: 0 } })
+}))
+
+export default useUserWordProgressStore
