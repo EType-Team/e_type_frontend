0a0d31480997c1e9189284af7106de0e4be9abbd
KaimuSeino <skim07a19yki@icloud.com>
2024-09-07 19:15:12 +0900

    CsrfProviderをラップ

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
+import CsrfProvider from "@/components/csrf-provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -24,7 +25,9 @@ export default function RootLayout({
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}>
+          <CsrfProvider>
         {children}
+          </CsrfProvider>
       </body>
     </html>
   )
