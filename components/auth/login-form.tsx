8d6d380ceca74bdbee2cd411a038130c2f0e2de2
KaimuSeino <skim07a19yki@icloud.com>
2024-09-08 11:08:39 +0900

    ログインフォームコンポーネントを作成

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,67 @@
+"use client"
+
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { Button } from "@/components/ui/button";
+import { useMutateAuth } from "@/hooks/useMutateAuth";
+import { FormEvent, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const LoginForm = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const { loginMutation } = useMutateAuth()
+
+    const submitLoginHandler = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        loginMutation.mutate({
+            email: email,
+            password: password
+        })
+    }
+    return (
+        <CardWrapper
+            headerLabel="おかえりなさい！"
+            backButtonLabel="アカウントをお持ちではありませんか？"
+            backButtonHref="/auth/register"
+        >
+            <form 
+                onSubmit={submitLoginHandler}
+                className="flex flex-col gap-y-4"
+            >
+                <div>
+                    <Label htmlFor="email">メールアドレス</Label>
+                    <Input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoFocus
+                        placeholder="etype@example.com"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
+                </div>
+                <div>
+                    <Label htmlFor="password">パスワード</Label>
+                    <Input
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="***********"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                    />
+                </div>
+            </form>
+            <Button
+                disabled={!email || !password}
+                type="submit"
+                className="w-full mt-4 bg-blue-500 hover:bg-blue-700"
+            >
+                ログイン
+            </Button>
+        </CardWrapper>
+    )
+}
+
+export default LoginForm;
