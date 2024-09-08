4ca6763c1efb5d757d790868fb82c6747131f8e4
KaimuSeino <skim07a19yki@icloud.com>
2024-09-08 11:12:42 +0900

    ログインフォームを追加

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/auth/login-form";
+
+const LoginPage = () => {
+    return (
+        <div className="mt-28">
+            <LoginForm />
+        </div>
+    );
+}
+ 
+export default LoginPage;
