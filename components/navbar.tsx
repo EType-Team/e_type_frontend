668751f9d565a66edccc58c5b46bfe1e91cc12b4
KaimuSeino <skim07a19yki@icloud.com>
2024-09-08 13:07:06 +0900

    navbarにUserAvatarを追加

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,11 @@
 import Logo from "@/components/logo"
+import UserAvatar from "./user-avatar"
 
 const Navbar = () => {
     return ( 
-        <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+        <div className="p-4 border-b h-full flex items-center bg-white shadow-sm justify-between">
             <Logo />
+            <UserAvatar />
         </div>
     )
 }
