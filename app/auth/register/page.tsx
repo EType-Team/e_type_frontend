7992f2f753422b70b401452c210f529efa2d1b9b
KaimuSeino <skim07a19yki@icloud.com>
2024-09-08 11:12:56 +0900

    新規登録フォームの追加

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "@/components/auth/register-form";
+
+const RegisterPage = () => {
+    return (
+        <div className="mt-28">
+            <RegisterForm />
+        </div>
+    );
+}
+ 
+export default RegisterPage;
