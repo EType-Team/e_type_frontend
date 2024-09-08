b5fd026efea97b5d46d40432eaf1da212e3ac216
KaimuSeino <skim07a19yki@icloud.com>
2024-09-08 11:08:52 +0900

    新規登録フォームコンポーネントを作成

---
+++ components/auth/register-form.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { Button } from "@/components/ui/button";
+import { useMutateAuth } from "@/hooks/useMutateAuth";
+import { FormEvent, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const RegisterForm = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const { loginMutation } = useMutateAuth()
+
+    const submitLoginHandler = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        loginMutation.mutate({
+            email: email,
+            password: password
+        })
+    }
+    return (
+        <CardWrapper
+            headerLabel="ETypeへようこそ"
+            backButtonLabel="ログインに戻る"
+            backButtonHref="/auth/login"
+        >
+            <form 
+                onSubmit={submitLoginHandler}
+                className="flex flex-col gap-y-4"
+            >
+                <div>
+                    <Label htmlFor="name">ユーザー名</Label>
+                    <Input
+                        id="name"
+                        name="name"
+                        type="text"
+                        autoFocus
+                        placeholder="ユーザ名"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={name}
+                    />
+                </div>
+                <div>
+                    <Label htmlFor="email">メールアドレス</Label>
+                    <Input
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="etype@example.com"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
+                </div>
+                <div>
+                    <Label htmlFor="password">パスワード</Label>
+                    <Input
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="***********"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                    />
+                </div>
+            </form>
+            <Button
+                disabled={!name || !email || !password}
+                type="submit"
+                className="w-full mt-4 bg-blue-500 hover:bg-blue-700"
+            >
+                新規作成
+            </Button>
+        </CardWrapper>
+    )
+}
+
+export default RegisterForm;
