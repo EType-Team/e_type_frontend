035e54a1392c90fb47c7dd075cbf3a6fb84425e8
KaimuSeino <skim07a19yki@icloud.com>
2024-09-09 22:41:14 +0900

    フェッチしたデータを使用する

--- app/(main)/(platform)/lesson/page.tsx
+++ app/(main)/(platform)/lesson/page.tsx
@@ -1,14 +1,23 @@
 import { PencilLine } from "lucide-react"
 import LessonCard from "./_components/lesson-card"
+import { cookies } from "next/headers"
+import { Lesson } from "@/types";
 
-const LessonPage = () => {
-    return ( 
+const LessonPage = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/lessons`, {
+        headers: { Cookie: cookies().toString() },
+    })
+    const data: Lesson[] = await res.json()
+    return (
         <div className="p-8">
+            {data.map((lesson) => (
             <LessonCard
+                key={lesson.id}
                 icon={PencilLine}
-                label="easy"
-                lessonId="1"
+                label={lesson.title}
+                lessonId={lesson.id}
             />
+            ))}
         </div>
     )
 }
