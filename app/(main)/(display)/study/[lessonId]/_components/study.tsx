06694d538502426c9ab3efa3ebdc14788e7fd019
KaimuSeino <skim07a19yki@icloud.com>
2024-09-14 21:41:25 +0900

    word状態のグローバル状態をリセットする

--- app/(main)/(display)/study/[lessonId]/_components/study.tsx
+++ app/(main)/(display)/study/[lessonId]/_components/study.tsx
@@ -5,6 +5,7 @@ import ReadyCard from "./ready-card"
 import Timer from "./timer"
 import PlayCard from "./play-card"
 import ResultCard from "./result-card"
+import { useTypingStore } from "@/store/typing-store"
 
 interface StudyProps {
     label: string
@@ -26,6 +27,13 @@ const Study = ({
 }: StudyProps) => {
     const [isActive, setIsActive] = useState(0)
     const [onEnd, setOnEnd] = useState(0)
+    const resetCompletedWords = useTypingStore((state) => state.resetCompletedWords);
+
+    const handleStart = () => {
+        resetCompletedWords();
+        setIsActive(1);
+    }
+    
     return (
         <>
             <Timer 
@@ -37,7 +45,7 @@ const Study = ({
                 <ReadyCard
                     label={label}
                     time={time}
-                    onStart={() => setIsActive(1)}
+                    onStart={handleStart}
                 />
             )}
             {isActive === 1 && onEnd === 0 && (
@@ -47,7 +55,9 @@ const Study = ({
                 />
             )}
             {isActive === 1 && onEnd === 1 && (
-                <ResultCard />
+                <ResultCard
+                    cookie={cookie}
+                />
             )}
         </>
     )
