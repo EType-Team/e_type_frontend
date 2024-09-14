ecbe049f7274e34b24a99e42f9d97ed2ffb83eb8
KaimuSeino <skim07a19yki@icloud.com>
2024-09-14 21:44:46 +0900

    wordIdsの配列からlengthを取得し合計英単語タイピング数を計算

--- app/(main)/(display)/study/[lessonId]/_components/result-card.tsx
+++ app/(main)/(display)/study/[lessonId]/_components/result-card.tsx
@@ -2,18 +2,23 @@
 
 import { useEffect, useState } from "react"
 import ResultContent from "./result-content"
+import { useTypingStore } from "@/store/typing-store"
 
 const ResultCard = () => {
     const [isResult, setIsResult] = useState(false)
+    const completedWordIds = useTypingStore((state) => state.completedWordIds);
+    const totalTypingNum = completedWordIds.length
 
     useEffect(() => {
-        setTimeout(() => setIsResult(true), 3000)
-    }, [])
+        setTimeout(() => setIsResult(true), 3000);
+    }, []);
 
     return (
         <>
             {isResult ? (
-                <ResultContent />
+                <ResultContent
+                    totalTypingNum={totalTypingNum}
+                />
             ) : (
                 <p className="text-3xl font-semibold">
                     終了！
@@ -23,4 +28,4 @@ const ResultCard = () => {
     );
 }
  
-export default ResultCard
+export default ResultCard;
