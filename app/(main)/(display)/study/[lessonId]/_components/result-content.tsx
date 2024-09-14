7f72af0fc4e225dfd285e594a13128466de53a07
KaimuSeino <skim07a19yki@icloud.com>
2024-09-14 21:41:01 +0900

    結果表示カードにはとりあえず英単語の合計タイピング数を表示

--- app/(main)/(display)/study/[lessonId]/_components/result-content.tsx
+++ app/(main)/(display)/study/[lessonId]/_components/result-content.tsx
@@ -1,33 +1,34 @@
-import { Button } from "@/components/ui/button"
-import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardContent,
-    CardFooter
-} from "@/components/ui/card"
-import Link from "next/link"
+'use client';
 
-const ResultContent = () => {
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
+import Link from "next/link";
+
+interface ResultContentProps {
+    totalTypingNum: number
+}
+
+const ResultContent = ({
+    totalTypingNum
+}: ResultContentProps) => {
     return (
-        <Card>
+        <Card className="w-[300px]">
             <CardHeader>
-                <CardTitle className="text-center">
-                    結果
-                </CardTitle>
+                <CardTitle className="text-center">結果</CardTitle>
             </CardHeader>
-            <CardContent>
-                どの単語を学習したかを表示する
+            <CardContent className="flex flex-col justify-center items-center overflow-y-auto max-h-[250px] p-10">
+                <p>合計タイピング数</p>
+                <p>{totalTypingNum}</p>
             </CardContent>
             <CardFooter className="justify-center">
                 <Button>
-                    <Link href='/lesson'>
+                    <Link href="/lesson">
                         レッスン一覧
                     </Link>
                 </Button>
             </CardFooter>
         </Card>
-    )
-}
- 
-export default ResultContent
+    );
+};
+
+export default ResultContent;
