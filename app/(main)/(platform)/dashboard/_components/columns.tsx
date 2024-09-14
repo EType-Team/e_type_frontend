ed7de08eb7e332fd16bdba98e4e0b80945a6e410
KaimuSeino <skim07a19yki@icloud.com>
2024-09-14 21:39:14 +0900

    ユーザーのword進捗状況を確認するためのテーブルを作成

---
+++ app/(main)/(platform)/dashboard/_components/columns.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { ColumnDef } from "@tanstack/react-table"
+import {
+    ArrowUpDown,
+    MoreHorizontal,
+    Pencil,
+    UserCheck,
+    FileCheck,
+} from "lucide-react"
+import Link from "next/link"
+import { Badge } from "@/components/ui/badge"
+import { cn } from "@/lib/utils"
+import { UserWordProgress } from "@/types"
+import { Progress } from "@/components/ui/progress"
+
+export const columns: ColumnDef<UserWordProgress>[] = [
+    {
+        accessorKey: "word.english",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    英単語
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+    },
+  {
+    accessorKey: "word.japanese",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+           日本語
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    }
+  },
+  {
+    accessorKey: "proficiency",
+    header: ({ column }) => {
+        return (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                熟練度
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        )
+    },
+    cell: ({ row }) => {
+        const proficiency = row.original.proficiency
+        return (
+            <div className="flex items-center">
+                <Progress value={proficiency} className="w-[200px]" />
+            </div>
+        )
+    }
+  }
+]
