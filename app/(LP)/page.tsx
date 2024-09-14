19f7c89553813cbae08fd01e2bcc3b1c4d8b1a76
KaimuSeino <skim07a19yki@icloud.com>
2024-09-14 22:10:41 +0900

    簡単なLPを作成

--- app/(LP)/page.tsx
+++ app/(LP)/page.tsx
@@ -1,8 +1,29 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default async function Home() {
 
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <div className="flex flex-col h-screen justify-center items-center">
+            <Image
+                src="/etype-lp.png"
+                alt="etype-lp"
+                width={500}
+                height={0}
+            />
+            <p className="text-2xl m-4 font-semibold">
+                新しい学習体験
+            </p>
+            <Button
+                variant="outline"
+            >
+                <Link
+                    href='/dashboard'
+                >
+                    はじめる
+                </Link>
+            </Button>
+        </div>
+    );
 }
