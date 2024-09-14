a2f9678e7da1f75100d7f313d6df46359163fba6
KaimuSeino <skim07a19yki@icloud.com>
2024-09-14 16:14:48 +0900

    useWordProgressデータをフェッチし表示

--- app/(main)/(platform)/dashboard/page.tsx
+++ app/(main)/(platform)/dashboard/page.tsx
@@ -1,4 +1,17 @@
-const DashboardPage = () => {
+import { List } from "lucide-react"
+import ProgressCard from "./_components/progress-card"
+import { cookies } from "next/headers"
+import { UserWordProgress } from "@/types"
+
+const DashboardPage = async () => {
+    const cookie = cookies().toString()
+    const userWordProgressData = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/userWordProgresses`, {
+        headers: { Cookie: cookie },
+        cache: "no-store"
+    })
+    const userWordProgress: UserWordProgress[] = await userWordProgressData.json()
+    const sortedUserWordProgress = userWordProgress.sort((a, b) => b.proficiency - a.proficiency);
+
     return (
         <div>
             ダッシュボードページ
