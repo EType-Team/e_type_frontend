103615f5efc7cf955861bc8cd714b695bd763676
KaimuSeino <skim07a19yki@icloud.com>
2024-09-15 17:38:18 +0900

    user情報をdbから取得する関数を定義

---
+++ data/user.ts
@@ -0,0 +1,19 @@
+import { User } from "@/types/user"
+import { cookies } from "next/headers"
+
+export const getUser = async (): Promise<User | null> => {
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user`, {
+            headers: { Cookie: cookies().toString() },
+            method: "GET",
+        })
+        if (!response.ok) {
+          throw new Error("ユーザー情報の取得に失敗しました。");
+        }
+        const user: User = await response.json();
+        return user;
+    } catch (err) {
+        return null
+    }
+  }
+  
