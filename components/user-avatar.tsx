22e541abb90ec24b7af654dfac3159383962cf9c
KaimuSeino <skim07a19yki@icloud.com>
2024-09-15 17:37:45 +0900

    google認証で取得したuser名とimageを表示

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -1,11 +1,23 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getUser } from "@/data/user";
 
-const UserAvatar = () => {
+interface UserAvatarProps {
+    name: string
+    image: string
+}
+
+const UserAvatar = async ({
+    name,
+    image
+}: UserAvatarProps) => {
     return (
-        <Avatar className="border">
-            <AvatarImage src="/user-icon.png" className="p-1" />
-            <AvatarFallback>ET</AvatarFallback>
-        </Avatar>
+        <div className="flex justify-center items-center p-2 rounded-md hover:bg-slate-100 hover:cursor-pointer">
+            <p className="mr-2">{name}</p>
+            <Avatar className="border">
+                <AvatarImage src={`${image || "/"}`} />
+                <AvatarFallback>ET</AvatarFallback>
+            </Avatar>
+        </div>
     );
 }
  
