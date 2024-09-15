bcd3a4c7fa415b45ba007494aa6c13bba2fb6c73
KaimuSeino <skim07a19yki@icloud.com>
2024-09-15 17:37:28 +0900

    navbarでuser情報を取得

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,28 @@
 import Logo from "@/components/logo"
 import UserAvatar from "./user-avatar"
+import { getUser } from "@/data/user"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
-const Navbar = () => {
+const Navbar = async () => {
+    const user = await getUser()
     return ( 
         <div className="p-4 border-b h-full flex items-center bg-white shadow-sm justify-between">
             <Logo />
-            <UserAvatar />
+            {user ? (
+                <UserAvatar
+                    name={user?.name}
+                    image={user?.image}
+                />
+            ): (
+                <Button>
+                    <Link
+                        href="/auth/login"
+                    >
+                        ログイン
+                    </Link>
+                </Button>
+            )}
         </div>
     )
 }
