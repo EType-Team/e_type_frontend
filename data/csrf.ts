5e383bfe0f5226c5c69679ca6fc10bd990c9867c
KaimuSeino <skim07a19yki@icloud.com>
2024-09-16 15:56:36 +0900

    csrfを取得する関数を定義

---
+++ data/csrf.ts
@@ -0,0 +1,7 @@
+import { CsrfToken } from "@/types"
+import axios from "axios"
+
+export const getCsrfToken = async () => {
+    const { data } = await axios.get<CsrfToken>(`${process.env.NEXT_PUBLIC_API_URL}/api/csrf`)
+    return data.csrf_token
+}
