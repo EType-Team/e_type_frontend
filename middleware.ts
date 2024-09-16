5ecfa2bafd7e00d67a4068952c95115996e37365
KaimuSeino <skim07a19yki@icloud.com>
2024-09-16 18:36:41 +0900

    フロントのmiddlewareではtokenの存在を確認するのみ

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,15 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
-import { jwtVerify } from 'jose'
- 
+
 export async function middleware(request: NextRequest) {
     const token = request.cookies.get('token')?.value
-    const secret = process.env.SECRET
 
     if (!token) {
         return NextResponse.redirect(new URL('/auth/login', request.url))
     }
-
-    try {
-        const secretKey = new TextEncoder().encode(secret);
-        await jwtVerify(token, secretKey)
-        return NextResponse.next();
-    } catch (err) {
-        return NextResponse.redirect(new URL('/auth/login', request.url));
-    }
+    return NextResponse.next();
 }
- 
+
 export const config = {
   matcher: ['/dashboard', '/lesson', '/settings', '/study/:path*']
 }
