dceeb1c1d7d8759427f29436e975bc9f39a19f5d
KaimuSeino <skim07a19yki@icloud.com>
2024-09-16 15:56:06 +0900

    サーバーコンポーネントとして扱い、データを取得するための変更を

--- app/(main)/(platform)/settings/page.tsx
+++ app/(main)/(platform)/settings/page.tsx
@@ -1,14 +1,17 @@
-"use client"
-
-import { LogOut } from "lucide-react"
+import { getUser } from "@/data/user"
 import LogoutCard from "./_components/logout-card"
+import RenameCard from "./_components/rename-card"
 
-const SettingsPage = () => {
+const SettingsPage = async () => {
+    const user = await getUser()
     return ( 
-        <div className="p-8">
+        <div className="p-8 flex">
             <LogoutCard
-                icon={LogOut}
                 label="logout"
+            />
+            <RenameCard
+                username={user?.name!}
+                label="rename"
             />
         </div>
     )
