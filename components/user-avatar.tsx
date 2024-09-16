68ca0cdbb5421758be8d8eef93a9aad1bf6e3867
KaimuSeino <skim07a19yki@icloud.com>
2024-09-16 16:01:48 +0900

    user-avatarにdialogのメニューを追加

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -1,23 +1,64 @@
+"use client"
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { getUser } from "@/data/user";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+import { BookCheck, Layout, Settings } from "lucide-react";
+import DropdownMenuContentsItem from "./dropdown-menu-contents-item";
 
 interface UserAvatarProps {
     name: string
     image: string
 }
 
+const setRoutes = [
+    {
+        icon: Layout,
+        label: 'ダッシュボード',
+        href: '/dashboard',
+    },
+    {
+        icon: BookCheck,
+        label: 'レッスン',
+        href: '/lesson',
+    },
+    {
+        icon: Settings,
+        label: '設定',
+        href: '/settings',
+    },
+]
+
 const UserAvatar = async ({
     name,
     image
 }: UserAvatarProps) => {
     return (
-        <div className="flex justify-center items-center p-2 rounded-md hover:bg-slate-100 hover:cursor-pointer">
-            <p className="mr-2">{name}</p>
-            <Avatar className="border">
-                <AvatarImage src={`${image || "/"}`} />
-                <AvatarFallback>ET</AvatarFallback>
-            </Avatar>
-        </div>
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <div className="flex justify-center items-center p-2 rounded-md hover:bg-slate-100 hover:cursor-pointer">
+                    <p className="mr-2">{name}</p>
+                    <Avatar className="border">
+                            <AvatarImage src={`${image || "/"}`} />
+                            <AvatarFallback>ET</AvatarFallback>
+                    </Avatar>
+                </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                {setRoutes.map((route) => (
+                    <DropdownMenuContentsItem
+                        key={route.href}
+                        icon={route.icon}
+                        label={route.label}
+                        href={route.href}
+                    />
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
     );
 }
  
