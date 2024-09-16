2ca5a494f6c51665b7d422ba0fc537109a306185
KaimuSeino <skim07a19yki@icloud.com>
2024-09-16 16:03:57 +0900

    csrfを取得する関数を取得する

--- components/csrf-provider.tsx
+++ components/csrf-provider.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { CsrfToken } from "@/types";
+import { getCsrfToken } from "@/data/csrf";
 import axios from "axios";
 import { useEffect } from "react";
 
@@ -11,11 +11,11 @@ const CsrfProvider = ({
 }) => {
     useEffect(() => {
         axios.defaults.withCredentials = true
-        const getCsrfToken = async () => {
-            const { data } = await axios.get<CsrfToken>(`${process.env.NEXT_PUBLIC_API_URL}/api/csrf`)
-            axios.defaults.headers.common['X-CSRF-Token'] = data.csrf_token
+        const setCsrfToken = async () => {
+            const csrfToken = await getCsrfToken()
+            axios.defaults.headers.common['X-CSRF-Token'] = csrfToken
         }
-        getCsrfToken()
+        setCsrfToken()
     }, [])
     return <>{children}</>;
 }
