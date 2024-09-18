9cb46306cef8c9af6f1d807737a01fd33bf5f00e
KaimuSeino <skim07a19yki@icloud.com>
2024-09-18 21:40:55 +0900

    エンドポイントを変更

--- data/csrf.ts
+++ data/csrf.ts
@@ -2,6 +2,6 @@ import { CsrfToken } from "@/types"
 import axios from "axios"
 
 export const getCsrfToken = async () => {
-    const { data } = await axios.get<CsrfToken>(`${process.env.NEXT_PUBLIC_API_URL}/api/csrf`)
+    const { data } = await axios.get<CsrfToken>(`${process.env.NEXT_PUBLIC_API_URL}/csrf`)
     return data.csrf_token
 }
