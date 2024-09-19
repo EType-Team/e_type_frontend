8220f217b7e842d31c505c5624f27c4eab436297
KaimuSeino <skim07a19yki@icloud.com>
2024-09-19 20:21:48 +0900

    lessonデータを取得できなかった場合の処理を追加

--- app/(main)/(platform)/lesson/page.tsx
+++ app/(main)/(platform)/lesson/page.tsx
@@ -1,24 +1,28 @@
 import { PencilLine } from "lucide-react"
 import LessonCard from "./_components/lesson-card"
-import { cookies } from "next/headers"
-import { Lesson } from "@/types";
+import { getLessons } from "@/data/lesson";
 
 const LessonPage = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/lessons`, {
-        headers: { Cookie: cookies().toString() },
-    })
-    const data: Lesson[] = await res.json()
+    const lessons = await getLessons()
     return (
-        <div className="p-8">
-            {data.map((lesson) => (
-            <LessonCard
-                key={lesson.id}
-                icon={PencilLine}
-                label={lesson.title}
-                lessonId={lesson.id}
-            />
-            ))}
-        </div>
+        <>
+            {lessons ? (
+                <div className="p-8">
+                    {lessons.map((lesson) => (
+                    <LessonCard
+                        key={lesson.id}
+                        icon={PencilLine}
+                        label={lesson.title}
+                        lessonId={lesson.id}
+                    />
+                    ))}
+                </div>
+            ) : (
+                <div className="p-8">
+                    レッスンデータを取得できませんでした。
+                </div>
+            )}
+        </>
     )
 }
  
