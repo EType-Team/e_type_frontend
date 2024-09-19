386df1987d13c74c5d911d0ae2f65fb01778f149
KaimuSeino <skim07a19yki@icloud.com>
2024-09-19 19:59:34 +0900

    getLessonById関数を追加

--- data/lesson.ts
+++ data/lesson.ts
@@ -12,3 +12,16 @@ export const getLessons = async (): Promise<Lesson[] | false> => {
         return false
     }
 }
+
+export const getLessonById = async (lessonId: number): Promise<Lesson | false> => {
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/lessons/${lessonId}`)
+        if (!response.ok) {
+            return false
+        }
+        const data: Lesson = await response.json()
+        return data
+    } catch (err) {
+        return false
+    }
+}
