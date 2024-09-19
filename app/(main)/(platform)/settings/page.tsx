fe97b17231ec0e60a43c76314cbfda693087d064
KaimuSeino <skim07a19yki@icloud.com>
2024-09-19 20:02:35 +0900

    未ログイン時の処理を追加

--- app/(main)/(platform)/settings/page.tsx
+++ app/(main)/(platform)/settings/page.tsx
@@ -5,15 +5,23 @@ import RenameCard from "./_components/rename-card"
 const SettingsPage = async () => {
     const user = await getUser()
     return ( 
-        <div className="p-8 flex">
-            <LogoutCard
-                label="logout"
-            />
-            <RenameCard
-                username={user?.name!}
-                label="rename"
-            />
-        </div>
+        <>
+            {user ? (
+                <div className="p-8 flex">
+                    <LogoutCard
+                        label="logout"
+                    />
+                    <RenameCard
+                        username={user?.name!}
+                        label="rename"
+                    />
+                </div>
+            ) : (
+                <div className="p-8">
+                    ログインしてください。
+                </div>
+            )}
+        </>
     )
 }
  
