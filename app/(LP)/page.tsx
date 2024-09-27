fa08353dee7ca662c16882a8f6541f4f0bcb7cbc
KaimuSeino <skim07a19yki@icloud.com>
2024-09-27 18:16:21 +0900

    イメージ削除

--- app/(LP)/page.tsx
+++ app/(LP)/page.tsx
@@ -17,13 +17,6 @@ export default function Home() {
 
                 <main className="flex-grow flex flex-col items-center justify-center text-center">
                     <div className="max-w-2xl mx-auto">
-                        <Image
-                            src="/etype-lp.png"
-                            alt="Typing app"
-                            width={500}
-                            height={500}
-                            className="object-contain mb-8 drop-shadow-lg"
-                        />
                         <h2 className="text-3xl font-extrabold mb-4">新しい学習体験</h2>
                         <p className="text-xl mb-8"></p>
 
