801745cba4291266eb7ece16d37484cf2098a8e9
KaimuSeino <skim07a19yki@icloud.com>
2024-09-28 18:57:29 +0900

    ResultTestContentコンポーネントのUIと正解数と不正解数を表示

--- app/(main)/(display)/study/[lessonId]/_components/result-test-content.tsx
+++ app/(main)/(display)/study/[lessonId]/_components/result-test-content.tsx
@@ -1,8 +1,41 @@
-const ResultTestContent = () => {
+import LoadingNavigateButton from "@/components/loading-navigate-button";
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card";
+
+interface ResultTestContentProps {
+    totalCorrectWordNum: number
+    totalNotCorrectWordNum: number
+}
+
+const ResultTestContent = ({
+    totalCorrectWordNum,
+    totalNotCorrectWordNum
+}: ResultTestContentProps) => {
     return (
-        <div>
-            テスト結果
-        </div>
+        <Card className="w-[300px]">
+            <CardHeader>
+                <CardTitle className="text-center">結果</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col justify-center items-center">
+                <p>正解</p>
+                <p className="text-2xl font-bold">{totalCorrectWordNum}</p>
+                <p>不正解</p>
+                <p className="text-2xl font-bold">{totalNotCorrectWordNum}</p>
+            </CardContent>
+            <CardFooter className="justify-center">
+                <LoadingNavigateButton
+                    variant="outline"
+                    url="/lesson"
+                    label="レッスン一覧に戻る"
+                    loadingLabel="読み込み中..."
+                />
+            </CardFooter>
+        </Card>
     );
 }
  
