8de78b2d0cdaa5be584ca91c688213f6b892552b
KaimuSeino <skim07a19yki@icloud.com>
2024-09-28 18:56:33 +0900

    正解のidと不正解のidの配列を取得し、正解数と不正解数を取得。ResultTestContentに渡す。

--- app/(main)/(display)/study/[lessonId]/_components/result-card.tsx
+++ app/(main)/(display)/study/[lessonId]/_components/result-card.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import ResultContent from "./result-content"
 import { useTypingStore } from "@/store/typing-store"
 import ResultTestContent from "./result-test-content"
+import { useTestTypingStore } from "@/store/test-typing-store"
 
 interface ResultCardProps {
     resultType: number
@@ -13,8 +14,13 @@ const ResultCard = ({
     resultType
 }: ResultCardProps) => {
     const [isResult, setIsResult] = useState(false)
-    const completedWordIds = useTypingStore((state) => state.completedWordIds);
+    const completedWordIds = useTypingStore((state) => state.completedWordIds)
+    const correctWordIds = useTestTypingStore((state) => state.correctWordIds)
+    const notCorrectWordIds = useTestTypingStore((state) => state.notCorrectWordIds)
+
     const totalTypingNum = completedWordIds.length
+    const totalCorrectWordNum = correctWordIds.length
+    const totalNotCorrectWordNum = notCorrectWordIds.length
 
     useEffect(() => {
         setTimeout(() => setIsResult(true), 3000);
@@ -28,7 +34,10 @@ const ResultCard = ({
                         totalTypingNum={totalTypingNum}
                     />
                 ) : resultType === 2 ? (
-                    <ResultTestContent />
+                    <ResultTestContent
+                        totalCorrectWordNum={totalCorrectWordNum}
+                        totalNotCorrectWordNum={totalNotCorrectWordNum}
+                    />
                 ) : null
             ) : (
                 <p className="text-3xl font-semibold">
