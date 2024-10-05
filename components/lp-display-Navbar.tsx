75c707dd28061d18d97c608007fe9e673f61c4f6
KaimuSeino <skim07a19yki@icloud.com>
2024-10-05 22:08:01 +0900

    プライバシーポリシーと利用規約用のnavbarを作成

---
+++ components/lp-display-Navbar.tsx
@@ -0,0 +1,11 @@
+import WhiteLogo from "./while-logo";
+
+const LPDisplayNavbar = () => {
+    return (
+        <div className="p-4 border-b h-full flex items-center bg-[#0079C9] shadow-sm justify-between">
+            <WhiteLogo />
+        </div>
+    );
+}
+ 
+export default LPDisplayNavbar;
