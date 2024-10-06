66212b831fc89c13576da241b67a08ad18014699
KaimuSeino <skim07a19yki@icloud.com>
2024-10-06 17:26:59 +0900

    footerスタイル修正

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,28 +2,21 @@ import Link from 'next/link'
 
 const Footer = () => {
     return (
-        <footer className="p-8 w-full bg-gray-500 text-white">
+        <footer className="p-8 w-full border-t">
             <div className="max-w-4xl mx-auto flex flex-col md:flex-row justify-between items-center">
                 <div className="mb-4 md:mb-0">
-                    <ul className="space-y-2">
-                        <li>
-                            <Link href="/operator">
-                                運営元
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/terms">
-                                利用規約
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/privacy">
-                                プライバシーポリシー
-                            </Link>
-                        </li>
+                    <ul className="flex items-center justify-center space-x-8">
+                        <Link href="/operator">
+                            運営元
+                        </Link>
+                        <Link href="/terms">
+                            利用規約
+                        </Link>
+                        <Link href="/privacy">
+                            プライバシーポリシー
+                        </Link>
                     </ul>
                 </div>
-
                 <div className="text-center mt-4 md:mt-0">
                     <p>&copy; 2024 EType. All rights reserved.</p>
                 </div>
